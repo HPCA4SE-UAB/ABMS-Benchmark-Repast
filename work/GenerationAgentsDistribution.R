f14671bd97b5425b83602cd328b8a308760e8693
Andreu Moreno i Vendrell <amoreno@euss.cat>
2020-07-10 10:24:31 +0200

    Colomap

--- work/GenerationAgentsDistribution.R
+++ work/GenerationAgentsDistribution.R
@@ -2,8 +2,8 @@ df <- read.table("node-100.data")
 
 require(ggplot2)
 
-p <- ggplot(df, aes(df$V1,df$V2)) + labs(x = "X") + labs(y = "Y") + labs(title = "Distribució agents RepastHPC")
-p <- p + stat_bin2d(bins = 20)
+p <- ggplot(df, aes(df$V1,df$V2)) + labs(x = "X") + labs(y = "Y") + labs(title = "RepastHPC agent distribution")
+p <- p + stat_bin2d(bins = 20) + scale_fill_gradient(low = "lightblue", high = "red")
 pdf("AgentsDistribRepast.pdf")
 plot(p)
 dev.off()
